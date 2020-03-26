b3a3843e9e16e01574eebd2d557f43a90c40d749
ap1919 <ap1919@ic.ac.uk>
2020-03-26 17:51:39 +0000

    FinishedA1A2

--- lab4/exam_q4/bit_utils.cpp
+++ lab4/exam_q4/bit_utils.cpp
@@ -1,13 +1,36 @@
+#include <string>
 #include "bit_utils.hpp"
 
 int total_bit_flips_in_word(uint64_t x, uint64_t y)
 {
-    // TODO
+
+int count=0;
+for(int i=0; i<64; i++){
+	if(x%2 != y%2){
+		count = count + 1;
+	}
+	x=x/2;
+	y=y/2;
+
+}
+
+return count;
+
 }
 
 vector<int> bit_flip_positions_in_vector(const vector<bool> &a, const vector<bool> &b)
 {
-    // TODO
+
+vector <int> indices;
+
+for(int i=0; i<a.size(); i++){
+
+	if(a[i]!=b[i]){
+		indices.push_back(i);
+	}
+}
+
+return indices;
 }
 
 vector<bool> read_bit_vector(istream &src)
