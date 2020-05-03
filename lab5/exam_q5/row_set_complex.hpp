9d8bf7282aab1a186fe83c033cda839681ef9e63
ep519 <ep519@imperial.ac.uk>
2020-05-03 14:12:07 +0100

    B2moreprocess

--- lab5/exam_q5/row_set_complex.hpp
+++ lab5/exam_q5/row_set_complex.hpp
@@ -1,6 +1,7 @@
- #ifndef row_set_float_hpp
+#ifndef row_set_float_hpp
 #define row_set_float_hpp
 
+#include <complex>
 #include "row_set.hpp"
 
 #include <vector>
@@ -11,8 +12,8 @@ class RowSetFloat
 private:
     int m_r;
     int m_c;
-    vector<vector<float>> m_A;
-    float m_B;
+    vector<vector<complex<double>>> m_A;
+    complex<double> m_B;
 public:
     virtual std::string FieldName() const;
 
@@ -60,98 +61,44 @@ public:
     virtual void InvBuffer() override;
     //{ m_B = 1 / m_B; }
 
-    virtual bool MaxAbsBuffer(int r, int c) override
-    {
+    virtual bool MaxAbsBuffer(int r, int c) override;
+/*    {
         if(abs(m_A[r][c]) > abs(m_B)){
             m_B=m_A[r][c];
             return true;
         }else{
             return false;
         }
-    }
+    }*/
 
-    virtual ostream &WriteBuffer(std::ostream &dst) const override
-    { return dst << m_B; }
+    virtual ostream &WriteBuffer(std::ostream &dst) const override;
+   // { return dst << m_B; }
 
-    virtual istream &ReadBuffer(std::istream &dst) override
-    { return dst >> m_B; }
+    virtual istream &ReadBuffer(std::istream &dst) override;
+   // { return dst >> m_B; }
 
-    virtual bool IsUnit(int r, int c) const override
-    { return m_A[r][c]==1; }
+    virtual bool IsUnit(int r, int c) const override;
+   // { return m_A[r][c]==1; }
     
-    virtual bool IsZero(int r, int c) const override
-    { return m_A[r][c]==0; }
+    virtual bool IsZero(int r, int c) const override;
+   // { return m_A[r][c]==0; }
     
-    virtual void SwapRows(int r1, int r2) override
-    { swap(m_A[r1], m_A[r2] ); }
+    virtual void SwapRows(int r1, int r2) override;
+   // { swap(m_A[r1], m_A[r2] ); }
 
-    virtual void MultiplyRow(int row) override
-    {
+    virtual void MultiplyRow(int row) override;
+/*    {
         for(int c=1; c<=m_c; c++){
             m_A[row][c] *= m_B;  
         }
-    }
+    }*/
 
-    virtual void AddMultipleOfRow(int row, int src_row) override
-    {
+    virtual void AddMultipleOfRow(int row, int src_row) override;
+  /*  {
         for(int c=1; c<=m_c; c++){
             m_A[row][c] += m_B * m_A[src_row][c];  
         }
-    }
+    }*/
 
-    virtual void ToReducedRowEchelonForm() override 
-    {
-        // This is a modification of the pseudo-code from the
-        // wikipedia page: https://en.wikipedia.org/wiki/Gaussian_elimination
-
-        int h = 1; /* Initialization of the pivot row 
-        int k = 1; /* Initialization of the pivot column 
-
-        while( h <= m_r && k <= m_c){
-            /* Find the k-th pivot: 
-            //i_max := argmax (i = h ... m, abs(A[i, k]))
-            int i_max = h;
-            for(int i=h; i<=m_r; i++){
-                if( abs(m_A[i][k]) > abs(m_A[i_max][k] )){
-                    i_max=i;
-                }
-            }
-
-            if( m_A[i_max][k] == 0){
-                /* No pivot in this column, pass to next column 
-                k = k+1;
-            }else{
-                /* Move the pivot row to the correct place 
-                swap(m_A[h], m_A[i_max]);
-
-                /* Normalise the pivot row value to 1 
-                float f = 1 / m_A[h][k];
-                for(int c = 1; c<=m_c; c++){
-                    m_A[h][c] *= f;
-                }
-                /* Force pivot value to exactly one 
-                m_A[h][k]=1;
-
-                /* Do for all rows except pivot: 
-                for(int i = 1 ; i<=m_r ; i++){
-                    if(i!=h){
-                        float f = m_A[i][k] / m_A[h][k];
-                        
-                        /* Subtract f * m_A[h]: 
-                        for(int j = k + 1 ; j<=m_c ; j++){
-                            m_A[i][j] -= f * m_A[h][j];
-                        }
-                        
-                        /* Set pivot column to exactly zero:
-                        m_A[i][k] = 0;
-                    }                    
-                }
-                /* Increase pivot row and column 
-                h = h + 1;
-                k = k + 1;
-            }
-     */
-
-
-}
+};
 #endif
